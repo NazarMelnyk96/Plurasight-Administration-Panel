8b7e25c02a504c6d8b70ae292a6661adffef2c03
Nazar Melnyk <melnyk@lun.ua>
2017-10-27 10:29:35 +0300

    add browserfy config

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,18 @@
 const gulp = require('gulp');
 const connect = require('gulp-connect'); //Runs a local dev server
 const open = require('gulp-open'); //Open a URL in a web browser
+const browserify = require('browserify'); //Bundles JS
+const reactify = require('reactify'); // Transforms React JSX to JS
+const source = require('vinyl-source-stream'); // Use conventional text stream with Gulp
 
-var config = {
+const config = {
     port: 2017,
     devBaseUrl: 'http:localhost',
     paths: {
         html: './src/*.html',
-        dist: './dist'
+        js: './src/**/*.js',
+        dist: './dist',
+        mainJs: './src/main.js'
     }
 };
 
@@ -33,8 +38,18 @@ gulp.task('html', function() {
         .pipe(connect.reload());
 });
 
+gulp.task('js', function() {
+   browserify(config.paths.mainJs)
+       .transform(reactify)
+       .bundle()
+       .on('error', console.error.bind(console))
+       .pipe(source('bundle.js'))
+       .pipe(gulp.dest(`${config.paths.dist}/scripts`))
+       .pipe(connect.reload());
+});
+
 gulp.task('watch', function() {
    gulp.watch(config.paths.html, ['html']);
 });
 
-gulp.task('default', ['html', 'open', 'watch']);
+gulp.task('default', ['html', 'js', 'open', 'watch']);

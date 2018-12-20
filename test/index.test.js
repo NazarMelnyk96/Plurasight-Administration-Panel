1aa82a46371c2d3cfa565351e21805308cf98d5d
naz <nazar3223@gmail.com>
2018-12-20 19:45:33 +0200

    fix test

--- test/index.test.js
+++ test/index.test.js
@@ -2,6 +2,6 @@ test('2 + 2 equal 4', () => {
   expect(2 + 2).toEqual(4);
 });
 
-test('2 + 2 equal 5', () => {
-  expect(2 + 2).toEqual(5);
+test('2 + 3 equal 5', () => {
+  expect(2 + 3).toEqual(5);
 });
